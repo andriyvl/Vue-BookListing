a44745df80e27ac4575ab3195be528a23ed6cc64
Andriy Vl <viychuk.a@gmail.com>
2018-11-28 21:12:02 +0100

    complete1

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1> {{titles}} </h1>
+        <h1> {{title}} </h1>
     </div>
 </template>
 
@@ -9,7 +9,7 @@ export default {
     name: 'BookList',
     data() {
         return {
-            titles: 'All Books'
+            title: 'All Books'
         } 
     }    
 }
