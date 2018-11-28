0476af33a37c2dc3da3a96cc0ece869eb4441193
Andriy Vl <viychuk.a@gmail.com>
2018-11-28 21:00:48 +0100

    Completed the module

---
+++ src/components/BookList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1> {{titles}} </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BookList',
+    data() {
+        return {
+            titles: 'All Books'
+        } 
+    }    
+}
+</script>
+
+<style>
+h1, h2 {
+    font-weight: normal;
+}
+</style>
